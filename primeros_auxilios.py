597b7ba01eddcf46f5ec30a7d7bdd6272ce90db2
DavidPToyo <davidpichardot31@hotmail.com>
2025-05-25 17:55:27 +0000

    avance

---
+++ primeros_auxilios.py
@@ -0,0 +1,21 @@
+#Programa de primeros auxilios.
+
+# Inicio
+
+# Responde a estimulos?
+
+# Si (Valora la necesidad de llevarlo al hospital más cercano) -> Fin del programa
+
+# No -> (Abrí la vía Aérea)
+
+# ¿Respira?
+
+# Si (Permitirle posición de suficiente ventilación) -> Fin del programa
+
+# No -> Administrar 5 ventilaciones y llamar a Ambulancia
+
+# ¿Signos de Vida?
+
+# Si -> Reevaluar a la espera de la Ambulancia -> ¿Llego la ambulancia? -> Si -> Fin del progrma
+
+# No -> Administrar Comprensiones torácicas hasta que llegue ambulancia -> ¿Llegó la ambulancia? -> No -> ¿Signos de Vida? (Vuelta al bucle)
