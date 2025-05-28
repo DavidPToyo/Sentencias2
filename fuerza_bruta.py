776d9a3fe97ca57c54b806a83efb897a1c99c4df
DavidPToyo <davidpichardot31@hotmail.com>
2025-05-28 19:55:12 +0000

    Listo fuerza bruta

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -4,3 +4,24 @@ from string import ascii_lowercase
 print("Programa para hackear contraseña")
 
 
+while True:
+    clave = getpass.getpass("Ingresas clave (sin letra 'ñ' ni caracteres especiales): ")
+
+    t = 0
+    e = 0
+
+    while True:
+        if e < len(clave):
+            for letra in ascii_lowercase:
+                t += 1
+
+                if letra == clave[e]:
+                    break
+
+            e += 1
+
+        else:
+            print(f"La clave ingresada es: {clave}")
+            print(f"La cantidad de veces intentadas fue: {t} veces")
+            break
+    break
